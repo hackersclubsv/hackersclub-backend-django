ca040d48ee3cc3111e0cd58714f59c4390e98c7f
Raj Sudharshan <sudharshann05@gmail.com>
2023-07-29 01:49:49 -0700

    Implement Error handling for email not sent during registration process

--- coengage/views.py
+++ coengage/views.py
@@ -1,5 +1,8 @@
 import random
+from datetime import timedelta
 
+import boto3
+from botocore.exceptions import BotoCoreError, ClientError
 from django.contrib.auth import get_user_model
 from django.core.mail import send_mail
 from django.utils import timezone
@@ -15,21 +18,37 @@ from .serializers import UserSerializer
 User = get_user_model()
 
 
-def SendEmail(username, otp, email):
-    send_mail(
-        subject="Your one-time password",
-        message="",
-        html_message=f"""
+def send_email_ses(username, otp, email):
+    ses = boto3.client("ses", region_name="us-west-1")
+    try:
+        response = ses.send_email(
+            Destination={
+                "ToAddresses": [email],
+            },
+            Message={
+                "Body": {
+                    "Html": {
+                        "Charset": "UTF-8",
+                        "Data": f"""
                             <p>Hello {username},</p>
                             <p>You requested a one-time password. Use this password to continue your process.</p>
                             <table width='100%'><tr><td style='text-align: center; font-size: 28px; font-weight: bold;'>{otp}</td></tr></table>
                             <p>If you didn't request this email, please ignore it.</p>
                             <p>-- Northeastern University Silicon Valley HackersClub</p>
-                          """,
-        from_email="vidyalathanataraja.r@northeastern.edu",
-        recipient_list=[email],
-        fail_silently=False,
-    )
+                        """,
+                    },
+                },
+                "Subject": {
+                    "Charset": "UTF-8",
+                    "Data": "Your one-time password",
+                },
+            },
+            Source="vidyalathanataraja.r@northeastern.edu",
+        )
+    except (BotoCoreError, ClientError) as error:
+        return {"success": False, "message": str(error)}
+    else:
+        return {"success": True, "message": response["MessageId"]}
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -52,14 +71,26 @@ class RegisterView(CreateAPIView):
         response = super().create(request, *args, **kwargs)
         user = User.objects.get(email=request.data["email"])
         refresh = RefreshToken.for_user(user)
-        SendEmail(user.username, user.otp, user.email)
+        email_response = send_email_ses(user.username, user.otp, user.email)
+
+        if not email_response["success"]:
+            return Response(
+                {
+                    "refresh": str(refresh),
+                    "access": str(refresh.access_token),
+                    **response.data,
+                    "email_error": email_response["message"],
+                },
+                status=status.HTTP_200_OK,
+            )
 
         return Response(
             {
                 "refresh": str(refresh),
                 "access": str(refresh.access_token),
                 **response.data,
-            }
+            },
+            status=status.HTTP_200_OK,
         )
 
 
@@ -121,9 +152,15 @@ class ResendOTP(APIView):
             )
         user.otp = str(random.randint(100000, 999999))
         user.otp_created_at = timezone.now()
+        user.otp_expiration = timezone.now() + timedelta(minutes=10)
         user.save()
 
-        SendEmail(user.username, user.otp, user.email)
+        email_response = send_email_ses(user.username, user.otp, user.email)
+        if not email_response["success"]:
+            return Response(
+                {"email_error": "OTP not sent", "message": email_response["message"]},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )
 
         return Response(
             {"status": "New OTP sent, please check your email."},
